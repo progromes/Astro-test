1625be1a56ed4846a7c16b7cb152f56ca70925d7
Pusto <super.snikers2013@yandex.ru>
2024-03-29 13:23:02 +0200

    init

---
+++ keystatic.config.ts
@@ -0,0 +1,51 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'cloud',
+    pathPrefix: 'prod',
+  },
+  cloud: {
+    project: 'astro-test/astro-test',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: {
+            directory: 'src/assets/images/posts',
+            publicPath: '../../assets/images/posts/',
+          },
+        }),
+      },
+    }),
+    page: collection({
+      label: 'Pages',
+      slugField: 'title',
+      path: 'src/content/pages/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: {
+            directory: 'src/assets/images/pages',
+            publicPath: '../../assets/images/pages/',
+          },
+        }),
+      },
+    }),
+  },
+});
